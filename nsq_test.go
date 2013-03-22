9ba4871db2096f3394d0a253dafe2e38d30999d6
Pieter Joost van de Sande <pj@born2code.net>
2013-03-22 21:39:09 +0100

    Add second client to test and refactor

--- nsq_test.go
+++ nsq_test.go
@@ -6,35 +6,56 @@ import (
 )
 
 type TestHander struct {
-	t      *testing.T
-	reader *nsq.Reader
+	t *testing.T
+	q *nsq.Reader
+
+	handleMessage chan *nsq.Message
 }
 
 func (handler TestHander) HandleMessage(message *nsq.Message) error {
-	handler.t.Logf("handling message: %s", message)
+	handler.handleMessage <- message
 	return nil
 }
 
+func (handler TestHander) Stop() {
+	handler.q.Stop()
+	<-handler.q.ExitChan
+}
+
 func TestClientCanSendSomething(t *testing.T) {
+	qClient1, err := createTestReader(t)
+	qClient2, err := createTestReader(t)
+	defer qClient1.Stop()
+	defer qClient2.Stop()
+
+	if err != nil {
+		t.Fatalf("error while creating client: %v", err.Error())
+	}
+}
+
+func stopClient(handler TestHander) {
+	handler.q.Stop()
+	<-handler.q.ExitChan
+}
+
+func createTestReader(t *testing.T) (*TestHander, error) {
 	addr := "127.0.0.1:4150"
 	topicName := "reader_test"
 	q, _ := nsq.NewReader(topicName, "ch")
 	q.VerboseLogging = true
 
-	handler := TestHander{
-		t:      t,
-		reader: q,
+	handler := &TestHander{
+		t:             t,
+		q:             q,
+		handleMessage: make(chan *nsq.Message),
 	}
 
 	q.AddHandler(handler)
 
 	err := q.ConnectToNSQ(addr)
 	if err != nil {
-		t.Fatalf(err.Error())
+		return nil, err
 	}
+
+	return handler, nil
 }
-
-// func setup() {
-// 	nsq.Register("test", "chan")//
-
-// }
